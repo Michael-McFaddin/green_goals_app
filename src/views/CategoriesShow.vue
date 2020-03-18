60f74fa482a8d2f5a81b8a53526df13a2b51d9db
mike <mikemcfaddin@gmail.com>
2020-03-18 17:17:34 -0400

    adds momentjs library to show relative time of post creation

--- src/views/CategoriesShow.vue
+++ src/views/CategoriesShow.vue
@@ -1,30 +1,46 @@
 <template>
   <div class="categories-show">
 
-    <h1>{{ category.name }}</h1>
-    <div v-for="post in category.posts">
+    <div class="container">
+      <h1>{{ category.name }}</h1>
+      Search by Title: <input type="text" v-model="titleFilter"><br>
+      <button v-bind:class="{'button': sortById == -1}" v-on:click="sortById = -1 ">Sort by new to old</button>
+      <button v-bind:class="{'button': sortById == 1}" v-on:click="sortById = 1 ">Sort by old to new</button> 
+    </div>
+
+    <div v-for="post in orderBy(filterBy(category.posts, titleFilter, 'title'), 'id', sortById)">
       <h2>{{ post.title }}</h2>
       <h4>{{ post.user_name }}</h4>
       <p>{{ post.body }}</p>
       <img :src="post.image" alt=""><br>
       <router-link v-bind:to="`/posts/${post.id}`">Details</router-link><br><br>
     </div>
- 
+
 
   </div>
 </template>
 
 <style>
+img {
+  width: 250px;
+}
+.button {
+  background-color: blue;
+  color: white;
+}
 </style>
 
 <script>
 import axios from 'axios';
+import Vue2Filters from 'vue2-filters';
 
 export default {
+  mixins: [Vue2Filters.mixin],
   data: function() {
     return {
       category: {},
-      posts: []
+      titleFilter: "",
+      sortById: -1
     };
   },
 
--- src/views/PostsIndex.vue
+++ src/views/PostsIndex.vue
@@ -4,8 +4,8 @@
     <div class="container">
       <h1>All Posts</h1>
       Search by Title: <input type="text" v-model="titleFilter"><br>
-      <button v-on:click="sortById = -1 ">Sort by newest post</button>
-      <button v-on:click="sortById = 1 ">Sort by oldest post</button>
+      <button v-bind:class="{'button': sortById == -1}" v-on:click="sortById = -1 ">Sort by new to old</button>
+      <button v-bind:class="{'button': sortById == 1}" v-on:click="sortById = 1 ">Sort by old to new</button>
     </div>
     
     <div v-for="post in orderBy(filterBy(posts, titleFilter, 'title'), 'id', sortById)">
@@ -13,6 +13,7 @@
       <h3>{{ post.category_name }}</h3>
       <h4>{{ post.user_name }}</h4>
       <img v-bind:src="post.image" alt=""><br>
+      <h5>Created: {{ relativeDate(post.created_at) }} ago</h5>
       <router-link v-bind:to="`/posts/${post.id}`">Details</router-link><br><br>
     </div>
 
@@ -23,10 +24,15 @@
 img {
   width: 250px;
 }
+.button {
+  background-color: blue;
+  color: white;
+}
 </style>
 
 <script>
 import axios from 'axios';
+import moment from 'moment';
 import Vue2Filters from 'vue2-filters';
 
 export default {
@@ -48,6 +54,10 @@ export default {
       });
   },
 
-  methods: {}
+  methods: {
+    relativeDate: function(date) {
+      return moment(date).startOf('day').fromNow(date);
+    }
+  }
 };
 </script>
