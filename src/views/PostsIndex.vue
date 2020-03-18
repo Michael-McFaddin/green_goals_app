0ad3730ca6c8ce18fcee3ccc2155a7a08e444c75
mike <mikemcfaddin@gmail.com>
2020-03-18 11:10:42 -0400

    adds search by title feature to index and sort by date created

--- src/views/PostsIndex.vue
+++ src/views/PostsIndex.vue
@@ -3,10 +3,12 @@
     
     <div class="container">
       <h1>All Posts</h1>
-      Search by title keyword: <input type="text" v-model="titleFilter">
+      Search by Title: <input type="text" v-model="titleFilter"><br>
+      <button v-on:click="sortById = -1 ">Sort by newest post</button>
+      <button v-on:click="sortById = 1 ">Sort by oldest post</button>
     </div>
     
-    <div v-for="post in filterBy(posts, titleFilter, 'title')">
+    <div v-for="post in orderBy(filterBy(posts, titleFilter, 'title'), 'id', sortById)">
       <h2>{{ post.title }}</h2>
       <h3>{{ post.category_name }}</h3>
       <h4>{{ post.user_name }}</h4>
@@ -32,7 +34,8 @@ export default {
   data: function() {
     return {
       posts: [],
-      titleFilter: ""
+      titleFilter: "",
+      sortById: -1
     };
   },
 
