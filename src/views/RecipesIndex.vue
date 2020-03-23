23a370c8fd627e7f1bb198a5ff1abc3fe30e4717
mike <mikemcfaddin@gmail.com>
2020-03-23 13:02:04 -0400

    adds nav bar with router links and a footer from the theme

--- src/views/RecipesIndex.vue
+++ src/views/RecipesIndex.vue
@@ -1,18 +1,36 @@
 <template>
   <div class="recipes-index">
 
+    <div class="container">
+      <h3>Try a Random Vegetarian Recipe</h3>
+      <button v-on:click="returnRecipes()">Get Recipe</button>
+      <div v-for="recipe in recipes">
+        <h2>{{ recipe.title }}</h2>
+        <img v-bind:src="recipe.image" alt="">
+        <h4>Prep time: {{ recipe.prep_time }} minutes</h4>
+        <h4>Serves: {{ recipe.servings }}</h4><br>
+        <h4>Ingredients:</h4>
 
-    <h1>Recipe Search</h1>
-    <button v-on:click="returnRecipes()">Get Recipes</button>
-    <div v-for="recipe in recipes">
-      <h2>{{ recipe.title }}</h2>
-      <img v-bind:src="recipe.image" alt="">
-      <h4>Prep time: {{ recipe.prep_time }} minutes</h4>
-      <h4>Serves: {{ recipe.servings }}</h4>
-      <div v-for="ingredient in recipe.ingredients">{{ ingredient.ingredient }}</div>
-      <!-- <div v-for="instruction in recipe.analyzed_instructions">{{ instruction.step }}</div> -->
-      <p>{{ recipe.instructions }}</p>
+        <div v-for="ingredient in recipe.ingredients">
+          <ul>
+            <li>
+              {{ ingredient.ingredient }}
+            </li>
+          </ul>
+        </div><br><br>
+        
+        <h4>Instructions:</h4>
+        <div v-for="instruction in recipe.analyzed_instructions">
+          <ul>
+            <li>
+              {{instruction.number}}. {{ instruction.step }}
+            </li>
+          </ul>
+        </div>
+        <!-- <p>{{ recipe.instructions }}</p> -->
+      </div>
     </div>
+    
   
 
   </div>
