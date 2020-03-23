a70aa2a68ebac2076113620dca6defaf4120c992
mike <mikemcfaddin@gmail.com>
2020-03-23 17:54:35 -0400

    adds theme to user show and new transparent header/nav bar

--- src/views/UsersShow.vue
+++ src/views/UsersShow.vue
@@ -1,11 +1,36 @@
 <template>
   <div class="users-show">
-    <h1>Hello {{ user.first_name }}!</h1>
+
+    <!-- Page Tittle Start -->
+    <section class="section-5 bg-dark">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-3 col-sm-6">
+            <div class="pdd-horizon-40">
+              <img class="img-responsive img-style-2 mrg-horizon-auto border-radius-round" src="assets/images/thumb/user.jpg" alt="">
+            </div>
+          </div>
+          <div class="col-md-3 col-sm-6">
+            <div class="text-white text-center-sm">
+              <h2 class="no-mrg-btm">{{ user.first_name }} {{ user.last_name }}</h2>
+              <h4>User name: {{ user.user_name }}</h4>
+              <h4>Email: {{ user.email }}</h4>
+              <div class="mrg-top-40">
+                <router-link v-bind:to="`/users/${user.id}/edit`" class="btn btn-md border-radius-6 btn-theme mrg-right-10">Edit User</router-link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+<!-- Page Tittle End -->
+
+
+    <!-- <h1>Hello {{ user.first_name }}!</h1>
     <h3>{{ user.first_name }} {{ user.last_name }}</h3>
     <h3>User Name: {{ user.user_name }}</h3>
     <h3>Email: {{ user.email }}</h3>
-
-    <router-link v-bind:to="`/users/${user.id}/edit`">Edit User</router-link>
+    <router-link v-bind:to="`/users/${user.id}/edit`">Edit User</router-link> -->
 
   </div>
 </template>
